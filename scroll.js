b215cb36f67b820b3870a4d8d488565613997779
bmit-tes <sabina.teleskumar@espas.ch>
2024-02-13 13:58:25 +0100

    base is now done for scrolling

--- scroll.js
+++ scroll.js
@@ -2,16 +2,16 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a'); 
 
 window.addEventListener('scroll'  ,() => {
-    sections.forEach(sec => {
+    sections.forEach(section => {
         let top = window.scrollY;
-        let offset = sec.offsetTop;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+        let offset = section.offsetTop;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
            });
         };
     });
