3820617aa84f98a70fd5608bce0dfc6f8515e262
bmit-tes <sabina.teleskumar@espas.ch>
2024-02-13 10:57:07 +0100

    small corrections

--- scroll.js
+++ scroll.js
@@ -1,7 +1,7 @@
 let sections = document.querySelectorAll('section'); 
 let navLinks = document.querySelectorAll('header nav a'); 
 
-window.onscroll = () => {
+window.addEventListener('scroll'  ,() => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop;
@@ -11,8 +11,8 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active')
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
            });
         };
     });
-};
+});
