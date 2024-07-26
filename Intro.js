19c0e0d4ab0d0651bdb0ceb228f3d202e455448a
bi-it-tes <sabina.teleskumar@espas.ch>
2024-07-26 09:43:23 +0200

    cleaned the code with Prettier

--- Intro.js
+++ Intro.js
@@ -1,30 +1,29 @@
-let intro = document.querySelector('.intro');
-let logoSpan = document.querySelectorAll('.logo');
-let navigation = document.querySelector('.navigation');
+let intro = document.querySelector(".intro");
+let logoSpan = document.querySelectorAll(".logo");
+let navigation = document.querySelector(".navigation");
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
 
     setTimeout(() => {
-        logoSpan.forEach((span, idx) => {
-            setTimeout(() => {
-                span.classList.add('active');
-            }, (idx + 1) * 400)
-        });
-
+      logoSpan.forEach((span, idx) => {
         setTimeout(() => {
-            logoSpan.forEach((span, idx) => {
-                setTimeout(() => {
-                    span.classList.remove('active');
-                    span.classList.add('fade');
-                }, (idx + 1) * 50);
-            });
-        }, 2000);
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 50);
+      });
+    }, 2000);
 
-        setTimeout(() => {
-            intro.style.top = '-100vh';
+    setTimeout(() => {
+      intro.style.top = "-100vh";
 
-            //navigation visible after intro
-            navigation.style.display = 'flex';
-        }, 2300);
-    });
+      //navigation visible after intro
+      navigation.style.display = "flex";
+    }, 2300);
+  });
 });
